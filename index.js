2181fbde1fcf8703397bfb5d46d45d1dd10c7cd2
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-04-08 14:48:39 +0300

    85% Done!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const apiKey='81969a18c4464c8ca39bdf8f38bb815d'
-// const newsContainer=document.getElementById('main-sectionId')
-// const searchBtn=document.getElementById('searchbtn')
-// const input=document.getElementById('theme-input')
-// let prompt='Daily'
+const apiKey='81969a18c4464c8ca39bdf8f38bb815d'
+const newsContainer=document.getElementById('main-sectionId')
+const searchBtn=document.getElementById('searchbtn')
+const input=document.getElementById('theme-input')
+let prompt='Daily'
 
 
 // // Function for  fetch data when enter on UI
@@ -103,4 +103,53 @@
 // })()
 
  
+// Function for  catch articles from API 
 
+async function persistDataForEntranceUi(){
+    try{
+       const URL=`https://newsapi.org/v2/everything?q=news&pageSize=20&apiKey=${apiKey}`
+       const response=await fetch (URL)
+       const data=await response.json()
+       const articles= await data.articles
+       return articles
+    }
+    catch(error){
+    console.log(error);
+    
+    }
+}
+
+// Calling on API data through IIF
+(async()=>{
+    try{
+        const articles=await persistDataForEntranceUi()
+        UIspreadshit(articles)
+    }
+    catch(error){
+        console.log(error);
+    }
+})()
+
+
+// Spreading API data on UI
+function UIspreadshit(articles){
+    articles.forEach((article)=>{
+        const div=document.createElement('div')
+        div.classList.add('main-section-div')
+
+        const img=document.createElement('img')
+        img.src=article.urlToImage
+
+        const title=article.content>0 ? article.content.slice(0,4)+'...' :article.title
+        const h6=document.createElement('h6') 
+        h6.textContent=title
+        const p=document.createElement('p')
+        p.textContent=title
+
+        div.appendChild(img)
+        div.appendChild(h6)
+        div.appendChild(p)
+
+        newsContainer.appendChild(div)
+    })
+}
